9194ebabcd9512bb26d48d96fae89f577cdbe1a1
unknown <morris821028@gmail.com>
2016-01-06 11:36:30 +0800

    update mount script

--- homework2/scripts/mnt.sh
+++ homework2/scripts/mnt.sh
@@ -1,3 +1,9 @@
-sudo mount rootfs-2.6.30.6.ext4 mnt
+sudo mount rootfs-dirty.ext4 mnt
 sudo cp test_weighted_rr/test_weighted_rr mnt/root/
+sudo cp test_weighted_rr/test_sjf mnt/root/
+sudo cp test_weighted_rr/test_rms mnt/root/
+sudo cp test_weighted_rr/run.sh mnt/root/
+sudo cp test_weighted_rr/run_sjf.sh mnt/root/
+sudo cp test_weighted_rr/run_rms.sh mnt/root/
 sudo umount mnt
+
